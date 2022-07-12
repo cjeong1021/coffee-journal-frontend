c55f03126ab958b1cd15af50912baa981fef7b35
Christopher <cjeong1021@gmail.com>
2022-07-12 14:29:19 -0400

    create react app

---
+++ coffee-journal/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+
+function App() {
+  return (
+    <div className='App'>
+      <Navbar />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
