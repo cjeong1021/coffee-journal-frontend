c74db611a60e9c15624a2c8f9d7f5a0f23ed2a9d
Christopher <cjeong1021@gmail.com>
2022-07-14 09:18:57 -0400

    style homepage

--- coffee-journal/src/App.js
+++ coffee-journal/src/App.js
@@ -2,12 +2,34 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
+  const [coffees, setCoffees] = useState([]);
+
+  const getData = () => {
+    axios
+      .get('http://localhost:8001/coffees/')
+      .then((res) => {
+        console.log(res.data);
+        setCoffees(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(getData, []);
   return (
     <div className='App'>
       <Navbar />
-      <Home />
+      <main>
+        <Routes>
+          <Route path='' element={<Home coffees={coffees} />} />
+        </Routes>
+      </main>
     </div>
   );
 }
--- coffee-journal/src/components/Home/Home.js
+++ coffee-journal/src/components/Home/Home.js
@@ -1,5 +1,41 @@
-import React from 'react';
+import './Home.css';
 
-export default function Home() {
-  return <div>Home</div>;
+export default function Home(props) {
+  const coffeeList = props.coffees.map((coffee) => {
+    return (
+      <div class='card rounded overflow-hidden shadow-lg m-5 lg:basis-1/4'>
+        <img
+          class='w-full'
+          src='https://www.worldatlas.com/r/w768/upload/12/f8/83/coffee-cup.jpg'
+          alt='Sunset in the mountains'
+        />
+        <div class='px-6 py-4'>
+          <div class='font-bold text-xl mb-2'>{coffee.name}</div>
+          <p class='text-gray-700 text-base'>{coffee.notes}</p>
+        </div>
+        <div class='px-6 pt-4 pb-2 flex-wrap'>
+          <span class='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'>
+            {coffee.roast}
+          </span>
+          <span class='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'>
+            {coffee.brew_method}
+          </span>
+          <span class='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'>
+            {coffee.origin}
+          </span>
+        </div>
+      </div>
+    );
+  });
+  return (
+    <div>
+      <div className='add-coffee'>
+        <div className='button_plus'></div>
+        <h1>Add Coffee</h1>
+      </div>
+      <div className='flex flex-col items-center lg:flex-row lg:flex-wrap'>
+        {coffeeList}
+      </div>
+    </div>
+  );
 }
